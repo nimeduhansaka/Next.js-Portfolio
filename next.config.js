1cfb8c486f5dd3f8ed3a5ed0a2f1fc559184f375
nimeduhansaka <nimedu.h.gbro@gmail.com>
2025-11-15 10:18:21 +0530

    Fixed for publish

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: 'export',
   basePath: '/Next.js-Portfolio',
   assetPrefix: '/Next.js-Portfolio/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
